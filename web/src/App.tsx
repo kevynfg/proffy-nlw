494dc6a2ea3e5e5e806b2163e7219768ae7f0a69
kevynfg <kevynredlotus@gmail.com>
2021-02-25 00:49:20 -0300

    - first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//import dessa forma faz ele importar diretamente pro javascript
+import './assets/styles/global.css'
+
+import Landing from './pages/Landing/index'
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
